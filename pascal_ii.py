52fbe5db3e7b68dcbc01aa3ce628690f191ff175
katlew <kat.lew.junk@gmail.com>
2015-10-26 15:29:05 -0400

    10.26 Pascal's Triangle II

--- pascal_ii.py
+++ pascal_ii.py
@@ -8,10 +8,22 @@ class Solution(object):
 		return res
 
 """
-rowIndex : 1 	res :[1]
+rowIndex : 1 	res :[1,1]
 
-rowIndex : 2	res : [1,1]
+rowIndex : 2	res : [1,2,1]
 
-rowIndex : 3	res : [1,1,1]
-	
+rowIndex : 3	res : [1,1,1,1]
+	row : 1 of 3
+	row : 2 of 3
+		col : 1 of 2
+			col = 2-1 = 1
+			res[1] = res[1] + res[0] = 1+1 = 2
+	row : 3 of 3
+		col : 1 of 3
+			col : 3-1 = 1
+			res[2] = res[2] + res[1] = 1 + 2 = 3
+		col : 2 of 3
+			col : 3-2 = 1
+			res[1] = res[1] + res[0] = 2 + 1 = 3
+[1,3,3,1]
 """
