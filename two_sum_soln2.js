9bee2578ebd0fa3efc76c759c5da42fd83af8497
Kathleen <kalew@microsoft.com>
2019-10-22 07:37:57 -0700

    js two_sum_soln2

---
+++ two_sum_soln2.js
@@ -0,0 +1,59 @@
+/**
+ * Two Sum
+ * Approach 1: Brute Force
+ * Given an array of integers, return indices of the two numbers such that 
+ * they add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, 
+ * and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ * A simple implementation uses two iterations. 
+ * In the first iteration, we add each element's value and its index to the table. 
+ * Then, in the second iteration we check if each element's complement 
+ * (target - nums[i]) 
+ * exists in the table. 
+ * Beware that the complement must not be nums[i] itself!
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var res = [];
+    d = {};
+    for(var i = 0; i < nums.length; ++i)
+    {
+        d[nums[i]] = i;
+    }
+
+    for(var i = 0; i < nums.length; ++i)
+    {
+        var complement = target - nums[i];
+        if (complement in d && d[complement] != i)
+        {
+            res.push(i);
+            res.push(d[complement]);
+            return res;
+        }
+    }
+};
+
+/*
+Complexity Analysis:
+
+Time Complexity : O(n).
+We traverse the list containing n elements exactly twice.
+Since the hash table reduces the look up time to O(1),
+the time complexity is O(n).
+
+Space complexity: O(n).
+The extra space required depends on the number of items stored 
+in the hash table, which stores exactly n elements
+*/
